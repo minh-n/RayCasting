159b3812112ae860373288fd00dbe8861f10cced
alavil1 <alavil1@a-137014.ups.u-psud.fr>
2017-11-29 14:39:51 +0100

    test

--- RayonTP/RayonTP/src/RayonTP.cpp
+++ RayonTP/RayonTP/src/RayonTP.cpp
@@ -11,6 +11,6 @@ using namespace std;
 
 //test
 int main() {
-	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	cout << "!!!Hello World fgd!!!" << endl; // prints !!!Hello World!!!
 	return 0;
 }
